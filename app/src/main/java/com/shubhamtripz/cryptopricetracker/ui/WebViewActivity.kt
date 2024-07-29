e42108d9d1d130f93631cd09a2ce25f41ef0aefe
backpackerdeveloper <contactshubhamtripz@gmail.com>
2024-07-29 13:37:17 +0530

    UI Improvement in main Activity

--- app/src/main/java/com/shubhamtripz/cryptopricetracker/ui/WebViewActivity.kt
+++ app/src/main/java/com/shubhamtripz/cryptopricetracker/ui/WebViewActivity.kt
@@ -3,6 +3,8 @@ package com.shubhamtripz.cryptopricetracker.ui
 import android.os.Bundle
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.shubhamtripz.cryptopricetracker.R
 import com.shubhamtripz.cryptopricetracker.databinding.ActivityWebViewBinding
 
 class WebViewActivity : AppCompatActivity() {
@@ -12,6 +14,17 @@ class WebViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityWebViewBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // -------- TOOLBAR CODE START -------
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        // Enable the back button
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        // Handle back button click
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+        // -------- TOOLBAR CODE END ---------
 
         val url = intent.getStringExtra("url")
         if (url != null) {
